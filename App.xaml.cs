1e61bcc49a2bf867331e382af7ce95d25bc16d3f
student-a502 <student-a502@STUD-RM21-A502>
2025-12-17 12:54:25 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Weather_8pr_permilka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
